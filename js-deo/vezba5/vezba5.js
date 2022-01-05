d95019fdc222c51a3b60e5440f9c5861f3eca804
Vahid Gasanin <vahid.gasanin1@gmail.com>
2022-01-05 19:11:09 +0100

    first commit

---
+++ js-deo/vezba5/vezba5.js
@@ -0,0 +1,10 @@
+//unos=prompt("Unesite neki string:");
+/*for (i = 7; i > 0; i--) {
+  console.log(i);
+}*/
+//tablica mnozenja:
+/*for (i = 1; i < 11; i++) {
+  for (j = 1; j < 11; j++) {
+    console.log("Proizvod", i, "i", j, "je", i * j);
+  }
+}*/
