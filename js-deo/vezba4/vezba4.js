358e0d9ded05ca1187a448c01e868eba2acef304
Vahid Gasanin <vahid.gasanin1@gmail.com>
2022-01-05 01:23:46 +0100

    first commit

--- js-deo/vezba4/vezba4.js
+++ js-deo/vezba4/vezba4.js
@@ -1,4 +1,14 @@
-a = prompt("Unesite neki string:");
+/*a = prompt("Unesite neki string:");
 for (i = 0; i < a.length; i++) {
   b = console.log(a.charAt((i += 0)));
 }
+*/
+/*b = 0;
+a = parseInt(prompt("Koliko brojeva zelite da unesete:"));
+for (i = 0; i < a; i++) {
+  unos = parseInt(prompt("Unesite broj:"));
+ // unos *= unos;
+  unos += unos;
+}
+console.log(unos);
+*/
