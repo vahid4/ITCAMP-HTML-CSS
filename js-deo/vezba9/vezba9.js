c798c470f051e945a29e22ae6b933dc2a5948d76
Vahid Gasanin <vahid.gasanin1@gmail.com>
2022-01-19 19:46:28 +0100

    first commit

--- js-deo/vezba9/vezba9.js
+++ js-deo/vezba9/vezba9.js
@@ -1,7 +1,28 @@
-unos = parseInt(prompt("koliko zelis elemenata da uneses"));
+/*unos = parseInt(prompt("koliko zelis elemenata da uneses"));
 ar = [];
 for (i = 0; i < unos; i++) {
   element = prompt("UNesite element");
   ar[i] = element;
 }
 console.log(ar);
+
+for (i = ar.lenght - 1; i >= 0; i--) {
+  console.log(ar[i]);
+}
+console.log(ar[i]);
+*/
+unos = parseInt(prompt("koliko zelis brojeva da uneses"));
+ar = [];
+//zbir = 0;
+for (i = 0; i < unos; i++) {
+  brojevi = parseInt(prompt("UNesite element"));
+  // zbir += brojevi;
+  ar[i] = brojevi;
+}
+console.log(ar);
+//console.log(zbir);
+zbir = 0;
+for (i = 0; i < ar.length; i++) {
+  zbir += ar[i];
+}
+console.log(zbir);
