27e90be263c311eb678f32bef69e3db02a68c58f
Vahid Gasanin <vahid.gasanin1@gmail.com>
2022-01-28 19:18:10 +0100

    first commit

--- js-deo/vezba13/vezba13.js
+++ js-deo/vezba13/vezba13.js
@@ -3,11 +3,19 @@
 }
 sayHello();
 */
+//-------------------------------------
+/*
 function zbir(params) {
   a = parseInt(prompt("unesite prvi broj:"));
   b = parseInt(prompt("unesite drugi  broj:"));
   sum = a + b;
   console.log("Zbir ova dva broja je", sum);
 }
-
 zbir();
+*/
+//-----------------------------------
+/*function sum(a, b) {
+  console.log(a + b);
+}
+sum(5, 7);
+*/
