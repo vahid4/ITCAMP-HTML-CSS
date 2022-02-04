e3ba50033b3520d5c5e86e7a28ac89d50a8854ce
Vahid Gasanin <vahid.gasanin1@gmail.com>
2022-02-04 19:13:54 +0100

    first commit

---
+++ js-deo/vezba16/vezba16.js
@@ -0,0 +1,36 @@
+/*function squareSum(numbers) {
+  kvad = 0;
+  sum = 0;
+  for (i = 0; i < numbers.length; i++) {
+    kvad = Math.pow(niz[i], 2);
+    sum += kvad;
+  }
+
+  return sum;
+}
+squareSum(1, 2, 2);
+*/
+//-------------------------------
+/*var niz = "Hi! HI! Hi! ";
+for (i = 0; i < niz.length; i++) {
+  if (niz[i] === "!") {
+    niz = niz.splice(i, 1);
+  }
+}
+niz.concat("!");
+console.log(niz);
+*/
+//ispisi u codewars
+//Abbreviate a Two Word Name
+/*function abbrevName(name) {
+  novi = "";
+  for (i = 0; i < name.length; i++) {
+    if (name[i] == " ") {
+      novi = name[0] + "." + name[i + 1];
+    }
+  }
+  console.log(novi);
+  return novi;
+}
+abbrevName("Vahid Gasanin");
+*/
