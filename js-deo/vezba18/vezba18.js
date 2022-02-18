2612b57bc492592fc294a3c0d71079c5f7e1381e
Vahid Gasanin <vahid.gasanin1@gmail.com>
2022-02-18 19:27:28 +0100

    first commit

--- js-deo/vezba18/vezba18.js
+++ js-deo/vezba18/vezba18.js
@@ -12,11 +12,38 @@ numbers.forEach(a);
 }
 numbers.forEach(a);
 */
-novi = [];
+//-----------------------
+/*novi = [];
 function a(b) {
   if (b > 20) {
     novi.push(b);
   }
 }
 numbers.forEach(a);
+console.log(novi);
+*/
+//--------------------------
+/*s = "the quick brown fox";
+s = s.split("");
+console.log(s);
+d = "";
+function a(b, c) {
+  if (c % 2 === 0) {
+    d += b.toLowerCase();
+  } else {
+    d += b.toUpperCase();
+  }
+}
+console.log(d);
+s.forEach(a);
+*/
+
+const arr = [NaN, 0, 15, false, -22, "", undefined, 47, null];
+novi = [];
+function a(b) {
+  if (Boolean(b) === false) {
+    novi.push(b);
+  }
+}
+arr.forEach(a);
 console.log(novi);
