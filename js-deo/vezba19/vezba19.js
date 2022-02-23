33f07006d0ba8fd3894a26f2d64318048de8df6b
Vahid Gasanin <vahid.gasanin1@gmail.com>
2022-02-23 18:26:03 +0100

    first commit

--- js-deo/vezba19/vezba19.js
+++ js-deo/vezba19/vezba19.js
@@ -1,5 +1,11 @@
-numbers = [45, 19, 74, 82, 14, 17, 16, 30, 35, 62];
+/*numbers = [45, 19, 74, 82, 14, 17, 16, 30, 35, 62];
 filtrirani = numbers.filter((el) => {
   return el > 18;
+});
+console.log(filtrirani);
+*/
+numbers = [45, 19, 74, 82, 14, 17, 16, 30, 35, 62];
+filtrirani = numbers.filter((el) => {
+  return el % 2 === 0;
 });
 console.log(filtrirani);
