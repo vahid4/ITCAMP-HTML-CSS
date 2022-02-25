07be00c9bef4b58fb994c366a89df7d1090d23ef
Vahid Gasanin <vahid.gasanin1@gmail.com>
2022-02-25 18:51:17 +0100

    first commit

--- js-deo/vezba20/vezba20.js
+++ js-deo/vezba20/vezba20.js
@@ -1,3 +1,11 @@
-const numbers = [18, 16, 2, 7, 9];
+/*const numbers = [18, 16, 2, 7, 9];
 const b = numbers.indexOf(7);
+console.log(b);
+*/
+
+const numbers = [18, 16, 2, 7, 9];
+const b = numbers.find((el) => {
+  return el % 2 === 0;
+});
+
 console.log(b);
