fb6a2323e2ae35b5155b7ffc54f58ba6986c0761
Vahid Gasanin <vahid.gasanin1@gmail.com>
2022-03-02 18:00:12 +0100

    first commit

--- js-deo/vezba20/vezba20.js
+++ js-deo/vezba20/vezba20.js
@@ -141,8 +141,8 @@ shifter(s);
 }
 */
 
-function toTime(seconds) {
+/*function toTime(seconds) {
   hours = Math.floor(seconds / 3600);
   minutes = Math.floor((seconds - hours * 3600) / 60);
   return hours + " hour(s) and " + minutes + " minute(s)";
-}
+}*/
---
+++ js-deo/vezba21/vezba21.js
@@ -0,0 +1,15 @@
+/*function XO(str) {
+  var xovi = 0;
+  var oovi = 0;
+  str = str.toLowerCase();
+  for (i = 0; i < str.length; i++) {
+    if (str[i] === "x") {
+      xovi++;
+    } else if (str[i] === "o") {
+      oovi++;
+    }
+  }
+  if (xovi === oovi) {
+    return true;
+  } else return false;
+}*/
