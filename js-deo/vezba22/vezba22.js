212abb755cf0f533d81a8fcd4dc0639a34f60a5f
Vahid Gasanin <vahid.gasanin1@gmail.com>
2022-03-11 18:50:44 +0100

    first commit

--- js-deo/vezba22/vezba22.js
+++ js-deo/vezba22/vezba22.js
@@ -1,3 +1,12 @@
 //09.03.2022.
 //osci model
 //https, dns, port
+
+console.log("aaaaaaaaaaaaaaaaaaaaaa");
+async function GetData() {
+  const response = await fetch("https://catfact.ninja/facts");
+  const responseJson = await response.json();
+  console.log(responseJson);
+}
+console.log("vvvvvvvvvvvvvvvvvvvv");
+GetData();
