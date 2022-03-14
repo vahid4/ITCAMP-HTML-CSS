6558639b3cbbe872a5697122e4c3519e5bdd7d36
Vahid Gasanin <vahid.gasanin1@gmail.com>
2022-03-14 19:53:00 +0100

    first commit

--- js-deo/vezba23/vezba23.js
+++ js-deo/vezba23/vezba23.js
@@ -1,12 +1,12 @@
 //console.log(document);
-const el = document.getElementById("heading");
+//const el = document.getElementById("heading");
 //const el = document.getElementsByClassName("main");
 //const el=document.getElementsByName();
 //const el = document.getElementsByTagName()
 //const il = document.querySelector("div");
 //console.log(el);
 //console.log(il);
-el.style.backgroundColor = "aqua";
+/*el.style.backgroundColor = "aqua";
 el.style.color = "gray";
 
 const il = document.getElementById("drugi");
@@ -20,3 +20,67 @@ sl.style.backgroundColor = "orange";
 console.log(el);
 console.log(il);
 console.log(sl);
+*/
+
+//const noviel = document.createElement("h1");
+//noviel.innerText = "novi element";
+//console.log(noviel);
+/*novi = "";
+async function GetData() {
+  const response = await fetch("https://catfact.ninja/facts");
+  const responseJson = await response.json();
+  console.log(responseJson);
+  for (i = 0; i < responseJson.length; i++) {
+    novi.push(responseJson[i].name);
+  }
+  console.log(novi);
+}
+
+GetData();
+*/
+
+/*novi = [];
+async function GetData() {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const responseJson = await response.json();
+    console.log(responseJson);
+    for (i = 0; i < responseJson.length; i++) {
+      novi.push(responseJson[i].name);
+    }
+    console.log(novi);
+  } catch (err) {
+    console.log(err);
+  }
+}
+GetData();
+*/
+/*async function GetData() {
+  const response = await fetch("https://catfact.ninja/facts");
+  const responseJson = await response.json();
+  console.log(responseJson);
+}
+GetData();
+*/
+
+async function GetData() {
+  novi = [];
+  main = document.getElementById("main");
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const responseJson = await response.json();
+    console.log(responseJson);
+    for (i = 0; i < responseJson.length; i++) {
+      novi.push(responseJson[i].name);
+    }
+    for (i = 0; i < novi.length; i++) {
+      const noviel = document.createElement("p");
+      noviel.innerText = novi[i];
+      main.appendChild(noviel);
+    }
+    console.log(novi);
+  } catch (err) {
+    console.log(err);
+  }
+}
+GetData();
