532fa5fac98b19c80654678aaf5ad7c234c67617
Vahid Gasanin <vahid.gasanin1@gmail.com>
2022-03-14 18:44:50 +0100

    first commit

--- js-deo/vezba23/vezba23.js
+++ js-deo/vezba23/vezba23.js
@@ -3,6 +3,7 @@ const el = document.getElementById("heading");
 //const el = document.getElementsByClassName("main");
 //const el=document.getElementsByName();
 //const el = document.getElementsByTagName()
-const il = document.querySelector("div");
+//const il = document.querySelector("div");
 console.log(el);
-console.log(il);
+//console.log(il);
+el.style.backgroundColor = "red";
